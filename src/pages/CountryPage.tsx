b3da959f0287431edf49e438946404e2a795e0ab
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-05-31 22:02:04 +0200

    feat: added back button on the country page component
    fix: moved the layout classes to the appropriate main page instead of main layout

--- src/pages/CountryPage.tsx
+++ src/pages/CountryPage.tsx
@@ -1,9 +1,15 @@
-import { useParams } from "react-router-dom"
+import { ArrowLeft } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export const CountryPage = () => {
-  const { countryName } = useParams<{ countryName: string }>();
 
   return (
-    <div>{countryName}</div>
+    <div className="lg:mt-1000">
+      <Link to="/" className="inline-flex items-center px-400 gap-2 py-100 rounded-md text-blue-500 bg-blue-900 dark:text-white mb-4 cursor-pointer">
+        <ArrowLeft />
+        Back
+      </Link>
+      <p>asddsadsa</p>
+    </div>
   )
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,30 +3,13 @@ import { SearchInput } from "../components/SearchInput"
 
 export const MainPage = () => {
   return (
-    <main className="dark:text-white text-gray-950 dark:bg-blue-950 bg-gray-50 min-h-screen">
-      
-        
-      <div className="flex flex-col py-500 px-5 gap-2 mb-8 md:flex-row md:justify-between md:items-center md:px-500 lg:px-1000">
-          
-          {/* Search Input */}
-          <div className="w-full md:flex-1 md:max-w-md mb-500 md:mb-0">
-            <SearchInput />
-          </div> 
-          
-          {/* Region Filter - Half width on mobile, fixed width on larger screens */}
-          <div className="w-1/2 md:w-80 md:ml-auto lg:w-96">
-            <RegionFilter />
-          </div>
-          
+      <>
+        <div className="w-full md:flex-1 md:max-w-md mb-500 md:mb-0">
+          <SearchInput />
+        </div>         
+        <div className="w-1/2 md:w-80 md:ml-auto lg:w-96">
+          <RegionFilter />
         </div>
-
-
-        {/* Countries Grid - Add your countries here */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {/* Your country cards will go here */}
-        </div>
-
-      
-    </main>
+      </>
   )
 }
