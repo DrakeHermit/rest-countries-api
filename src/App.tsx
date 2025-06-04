4c17d6d68e59a4dd58d157f6cc345c92e6851b46
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-06-04 12:52:33 +0200

    feat: added a query client and started working on fetching the county data

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,50 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import { MainLayout } from "./components/MainLayout"
-import { CountryPage } from "./pages/CountryPage"
-import { MainPage } from "./pages/MainPage"
+import { MainLayout } from "./components/MainLayout";
+import { CountryPage } from "./pages/CountryPage";
+import { MainPage } from "./pages/MainPage";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+const allCountriesLoader = async () => {
+  await queryClient.prefetchQuery({
+    queryKey: ["countries"],
+    queryFn: async () => {
+      const response = await fetch("https://restcountries.com/v3.1/all");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    },
+  });
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [{
-      index: true,
-      element: <MainPage />
-    },
-    {
-      path: "country/:countryName",
-      element: <CountryPage />  
-    }
-  ]
-  }
-])
+    loader: allCountriesLoader,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: "country/:countryName",
+        element: <CountryPage />,
+      },
+    ],
+  },
+]);
+
+console.log(allCountriesLoader);
 
 function App() {
-
-  return <RouterProvider router={router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
